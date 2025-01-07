0c939ce044dcbd87bb652b74647bd802219dde54
Vladimir <letuchikh@yandex.ru>
2025-01-07 15:28:05 +0600

    Commit 2

--- src/test/java/GitHubHW.java
+++ src/test/java/GitHubHW.java
@@ -1,6 +1,8 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
@@ -14,19 +16,12 @@ public class GitHubHW {
         Configuration.pageLoadStrategy = "eager";
     }
 
-    @org.junit.jupiter.api.Test
-    void fillFormTest() {
+    @Test
+     void fillFormTest() {
         open("");
-        // Ожидаем, что кнопка "Solutions" будет видна перед кликом
         $(byText("Solutions")).shouldBe(visible).click();
-
-        // Ожидаем, что кнопка "Enterprises" будет видна перед кликом
         $(byText("Enterprises")).shouldBe(visible).click();
-
-        // Ожидаем, что элемент с id "hero-section-brand-heading" будет видим
         $("#hero-section-brand-heading").shouldBe(visible).hover();
-
-        // Ожидаем, что текст элемента соответствует ожидаемому
         $("#hero-section-brand-heading").shouldHave(Condition.text("The AI-powered\n developer platform"));
     }
 }
